a4d59c5ba10a4cd1585166122771a4de0f9deb18
Subash Subramaniyan <93960578+subash-subramaniyan@users.noreply.github.com>
2025-09-15 17:47:28 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "RG-INC-STG-01"
+    resource_group_name  = "rg-ari-ins-p-storage-01"
     storage_account_name = "ariyans"
     container_name       = "terraform"
     key                  = "CloudOps.tfstate"
